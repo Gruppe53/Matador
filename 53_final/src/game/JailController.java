9e4f6cc7908da2ebbda9889d8be5a420b72128fa
Lars <larspeter94@gmail.com>
2014-01-13 16:53:28 +0100

    Added jail...

---
+++ 53_final/src/game/JailController.java
@@ -0,0 +1,18 @@
+package game;
+
+public class JailController {
+	public boolean isInJail = false;{
+	
+	if(isInJail == true){
+		//give choice
+		}
+	}
+
+	public boolean isInJail() {
+		return isInJail;
+	}
+
+	public void setInJail(boolean isInJail) {
+		this.isInJail = isInJail;
+	}
+}
