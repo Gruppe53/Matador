0817af7cb7f6d2b0c382268e2e45f1e606669c2e
Kristin Hansen <maagenator@gmail.com>
2014-01-14 15:37:21 +0100

    Chancekort ved at være færdige... tror stadig der er problemer med flere forskellige instanser af decks. :<

--- 53_final/src/game/ChanceDeck.java
+++ 53_final/src/game/ChanceDeck.java
@@ -6,22 +6,97 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Random;
 
-public class ChanceDeck {
+import boundaryToMatador.GUI;
+
+public class ChanceDeck extends Field {
 	private ChanceCard[] deck = new ChanceCard[46];
 	private int cardCount = 0;
 	private int pickCount = 0;
+	private String name;
 	
 	// FINALS
 	private final File file = new File("materials/chance.txt");
 	
-	public ChanceDeck() {
+	public ChanceDeck(String name) {
+		super(name);
 		createCards();
-		System.out.println(deck[44].getDescription() + " ---- test: \u00E5");
-		System.out.println(deck[45].getDescription());
 		shuffleDeck(deck);
-		System.out.println(deck[0].getDescription());
-		shuffleDeck(deck);
-		System.out.println(deck[0].getDescription());
+	}
+	
+	public void landOnField(Player player) {
+		GUI.showMessage("Tr\u00F6k et \"pr\uF008Fv lykken\"-kort");
+		GUI.showMessage(deck[pickCount].getDescription());
+        
+		switch(Integer.parseInt(deck[pickCount].getType())) {
+		case 0:
+			// Matadorlegat
+			break;
+		case 1:
+			// Modtag penge
+			player.alterAccount(Integer.parseInt(deck[pickCount].getAction()));
+			break;
+		case 2:
+			// Modtag penge, af hver spiller
+			// Skal lige........... umiddelbart ret svaer at lave, uden at bryde alle designprincipper
+			break;
+		case 3:
+			// Betal penge
+			player.alterAccount(Integer.parseInt(deck[pickCount].getAction()));
+			break;
+		case 4:
+			// Ryk brik
+			int newPosition = player.getPosition() + Integer.parseInt(deck[pickCount].getAction());
+			
+			// Hvis "ryk tre felter tilbage" traekkes, og man staar paa felt 2, fikser vi lige manuelt position
+			if(newPosition == -1) {
+				newPosition = 40;
+			}
+			
+			GUI.removeAllCars(player.getName());
+			GUI.setCar(newPosition, player.getName());
+			
+			break;
+		case 5:
+			// Ryk direkte
+			// Naar man traekker disse kort, og man skal rykke til et felt, og det felt er ejet, har vi et problem
+			// - selv hvis det IKKE er ejet, kan det ikke tilbydes til spilleren...
+			// Vi har nemlig fra ChanceDeck ingen kendskab til hverken de andre fields eller til andre spillere,
+			// og vi kan ikke kalde landOnField her fra
+			// - samme problem som med "Modtag penge, af hver spiller", hvor de andre spillere ikke er bekendte
+			
+			
+			if(Integer.parseInt(deck[pickCount].getAction()) == 11) {
+				// Ryk i faengsel - kan i det mindste laves... ^_^
+				player.setJailed(true);
+			}
+			
+			GUI.removeAllCars(player.getName());
+			GUI.setCar(Integer.parseInt(deck[pickCount].getAction()), player.getName());
+			break;
+		case 6:
+			// Ryk direkte og betal
+			// Igen... ingen kendskab...
+			break;
+		case 7:
+			// Ryk til rederi
+			// Ingen kendskab.
+			break;
+		case 8:
+			// Betal pr. hus/hotel
+			String[] str = deck[pickCount].getAction().split("xx");
+			int houses = Integer.parseInt(str[0]);
+			int hotels = Integer.parseInt(str[1]);
+			
+			player.alterAccount(-(player.getHouses() * houses));
+			player.alterAccount(-(player.getHotels() * hotels));
+			break;
+		case 9:
+			// Bail
+			player.setBailoutcards(1);
+			break;
+		default:
+				break;
+		}
 	}
 	
 	private void createCards() {
