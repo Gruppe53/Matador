2020580a755a1a3aa79c6cff43b40038a48e49b1
Kristin Hansen <maagenator@gmail.com>
2014-01-17 14:10:08 +0100

    Ved at være der...

--- 53_final/src/game/PropertyControl.java
+++ 53_final/src/game/PropertyControl.java
@@ -7,7 +7,7 @@ public class PropertyControl {
 	private Player player;
 	private boolean notDone = true;
 	
-	public PropertyControl(Player player, Street[] fields) {
+	public PropertyControl(Player player, Street[] fields, int fieldNumbers) {
 		this.player = player;
 		this.fields = fields;
 		
@@ -20,16 +20,33 @@ public class PropertyControl {
 	}
 	
 	private void buildEvenly(Street[] fields) {
-		String[] options = new String[3];
+		String[] options = new String[4];
 		
+		// !!!!!!!!!Hvis alle er ens, lav exception!!!!!!!!!!!!
+		// HUSK AT MAN MAKS KAN HAVE 4 HUSE (5 FOR HOTEL)
 		for(int i = 0; i < fields.length; i++) {
 			if(fields[i].getHouses() < highestCount(fields))
-				options[i] = fields[i].getName();
+				options[i] = (i + 1) + ". " + fields[i].getName();
 		}
 		
-		GUI.getUserSelection("Pris pr. hus: " + fields[0].getHousePrice(), options);
-		GUI.getUserButtonPressed("Pris pr. hus: " + fields[0].getHousePrice(), options);
+		options[4] = "4. Færdig med køb af huse.";
 		
+		String choice = GUI.getUserButtonPressed("Pris pr. hus: " + fields[0].getHousePrice(), options);
+		
+		switch(getChoice(choice)) {
+		case 1:
+			fields[0].setHouses(field, houses);
+			break;
+		case 2:
+			
+			break;
+		case 3:
+			
+			break;
+		case 4:
+			notDone = false;
+			break;
+		}
 		// Husk at opdatere assets
 	}
 	
@@ -41,5 +58,9 @@ public class PropertyControl {
 				highestCount = a.getHouses();
 		
 		return highestCount;
+	}
+	
+	private int getChoice(String str) {
+		return Integer.parseInt(str.split("\\. ")[0]);
 	}
 }
