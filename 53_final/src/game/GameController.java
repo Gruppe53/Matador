5cc190f5928a7f4ceab29c0f145e35c8b678a02e
Bakhoj <Anders2108@hotmail.com>
2014-01-18 18:39:09 +0100

    asd

--- 53_final/src/game/GameController.java
+++ 53_final/src/game/GameController.java
@@ -10,7 +10,7 @@ public class GameController {
 	private Dice roll = new Dice(1, 6, 2);
 	private boolean secondTurn = false;
 	private int multiplePair = 0;
-	private boolean createAuction = false;
+	private boolean createAuction = true;
 	// TODO fix "vend tilbage til hovedmenuen"
 	// FINALSs
 	private final int startCash = 30000;
