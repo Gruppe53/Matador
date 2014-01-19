5447bd7992a9169ddb498d13b24c548b7951d7a6
Lars <larspeter94@gmail.com>
2014-01-19 18:34:01 +0100

    w00t

--- 53_final/src/game/PropertyControl.java
+++ 53_final/src/game/PropertyControl.java
@@ -5,7 +5,6 @@ public class PropertyControl {
 	
 	public PropertyControl(Player player, Street[] fields, int[] fieldNumbers, Updater updater) {
 		for(int i = 0; i < fields.length; i++)
-			System.out.println("pControl " + (i + 1) + ": " + fields[i].getName());
 		// Secure evenly built houses
 		do {
 			this.buildEvenly(player, fields, fieldNumbers, updater);
